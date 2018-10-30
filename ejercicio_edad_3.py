0e9c6565b8aef59074b89c9562b52fba7d327ab0
Ambrior5806 <44443030+Ambrior5806@users.noreply.github.com>
2018-10-30 13:39:06 +0100

    Create ejercicio_edad_3.py

---
+++ ejercicio_edad_3.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+edad= int(input("por favor indique su edad")
+if ((edad>=18)and(edad<=23))or(edad=17):
+    print("puedes entrar wn la sesión de jovenes")
+else
+    print ("No puedes entrar, lo siento")
